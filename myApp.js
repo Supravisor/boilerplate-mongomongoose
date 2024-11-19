f8505dd020306b31ad96409729455e00fa3fa322
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-11-19 18:24:33 +1300

    create a model

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const personSchema = new mongoose.Schema({
+let personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
